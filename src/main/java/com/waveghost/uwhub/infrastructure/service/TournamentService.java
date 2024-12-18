7828d3113f563fbd3d1ecfe4308f4eaabdb3f92b
Alejandro Giraldo Serna <giraldosernaalejandro@gmail.com>
2024-12-18 18:01:12 -0500

    TournamentServiceImpl was implemented

--- src/main/java/com/waveghost/uwhub/infrastructure/service/TournamentService.java
+++ src/main/java/com/waveghost/uwhub/infrastructure/service/TournamentService.java
@@ -2,13 +2,16 @@ package com.waveghost.uwhub.infrastructure.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.waveghost.uwhub.api.dtos.request.TournamentRQ;
 import com.waveghost.uwhub.api.dtos.response.TournamentRS;
+import com.waveghost.uwhub.domain.entities.Tournament;
 import com.waveghost.uwhub.domain.repositories.TournamentRepository;
 import com.waveghost.uwhub.infrastructure.abstarct_service.ITournamentService;
+import com.waveghost.uwhub.utils.exceptions.IdNotFoundException;
 import com.waveghost.uwhub.utils.mappers.TournamentMapper;
 
 import lombok.AllArgsConstructor;
@@ -25,32 +28,47 @@ public class TournamentService implements ITournamentService
 
     @Override
     public TournamentRS create(TournamentRQ request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        Tournament tournament = this.tournamentMapper.requestToEntity(request);
+        
+        Tournament tournamentSaved = this.tournamentRepository.save(tournament);
+
+        return this.tournamentMapper.entityToResponse(tournamentSaved);
     }
 
     @Override
     public List<TournamentRS> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        return this.tournamentMapper.entityToResponse(this.tournamentRepository.findAll());
     }
 
     @Override
     public TournamentRS findById(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        Tournament tournament = this.find(id);
+
+        return this.tournamentMapper.entityToResponse(tournament);
     }
 
     @Override
     public TournamentRS update(TournamentRQ requets, String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        Tournament newTournament =  this.tournamentMapper.requestToEntity(requets);
+        Tournament actualTournament = this.find(id);
+        BeanUtils.copyProperties(newTournament, actualTournament);
+
+        Tournament tournamentUpdated = this.tournamentRepository.save(actualTournament);
+
+        return this.tournamentMapper.entityToResponse(tournamentUpdated);
     }
 
     @Override
     public void delete(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        Tournament tournament = this.find(id);
+        
+        this.tournamentRepository.delete(tournament);
+    }
+
+    public Tournament find(String id){
+        return this.tournamentRepository.findById(id).orElseThrow(
+            () -> new IdNotFoundException("Tournament", id.toString())
+        );
     }
     
 }
