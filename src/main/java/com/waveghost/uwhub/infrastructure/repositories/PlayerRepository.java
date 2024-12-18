69e34500d8c3222de3a2e974eeff35d02e3c75c8
Alejandro Giraldo Serna <giraldosernaalejandro@gmail.com>
2024-12-18 16:26:13 -0500

    Annotation @Repository was added to the repositories

--- src/main/java/com/waveghost/uwhub/infrastructure/repositories/PlayerRepository.java
+++ src/main/java/com/waveghost/uwhub/infrastructure/repositories/PlayerRepository.java
@@ -1,9 +1,11 @@
 package com.waveghost.uwhub.infrastructure.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.waveghost.uwhub.infrastructure.entities.Player;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, String>{
     
 }
--- src/main/java/com/waveghost/uwhub/infrastructure/repositories/TeamRepository.java
+++ src/main/java/com/waveghost/uwhub/infrastructure/repositories/TeamRepository.java
@@ -1,7 +1,9 @@
 package com.waveghost.uwhub.infrastructure.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamRepository extends JpaRepository<TeamRepository, Integer>{
     
 }
