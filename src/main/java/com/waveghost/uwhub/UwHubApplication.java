d0aeb6a5a357e1649ce43c8f8669ac18732a87cf
Alejandro Giraldo Serna <giraldosernaalejandro@gmail.com>
2024-12-18 15:04:37 -0500

    The folders of the project was added

---
+++ src/main/java/com/waveghost/uwhub/UwHubApplication.java
@@ -0,0 +1,13 @@
+package com.waveghost.uwhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UwHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waveghost/uwhub/UwHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waveghost.uwhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
